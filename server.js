175b8b103a41b0b5a86ad23e34f4eba0cb3c0121
Marco Fabian <marcofabianufmg@hotmail.com>
2025-07-13 08:28:22 -0300

    feat: configurar servidor Express básico com middlewares

--- server.js
+++ server.js
@@ -1,12 +1,17 @@
 const express = require('express')
+const path = require('path')
 
 const app = express();
 const PORT = 3000;
+
+app.use(express.static('public')); 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 app.get('/', (req, res) => {
     res.send("Hello World!");
 });
 
 app.listen(PORT, () => {
-    console.log(`Servidor da DevBurger rodando em localhost:${PORT}`);
+    console.log(`🍔 Servidor da DevBurger rodando em http://localhost:${PORT}`);
 });
